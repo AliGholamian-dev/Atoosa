0040cd0e0e341f43b2ad55fcf0650f4c0e03534c
Sepehr <alisepehr88@gmail.com>
2026-02-01 20:30:29 +0100

    Added CMakeLists.tx
    Signed-off-by: Sepehr <alisepehr88@gmail.com>

---
+++ Atoosa.c
@@ -0,0 +1,8 @@
+#include "Atoosa/Atoosa.h"
+#include "Common/NameManglingGuard.h"
+
+Common_NameManglingGuard_Begin
+
+
+
+Common_NameManglingGuard_End
---
+++ Atoosa.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Common/CompilerDetection.h"
+#include "Common/NameManglingGuard.h"
+
+Common_NameManglingGuard_Begin
+
+
+
+Common_NameManglingGuard_End
